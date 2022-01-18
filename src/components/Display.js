9497f27e2772f5ef85d89fcf73965867088b1ce0
Lizandro Martin <tabletemilianomartin2@gmail.com>
2022-01-17 23:08:22 -0600

    updated

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,46 +1,38 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Display = () => {
     const [operation, setOperation] = useState("");
-    const numbers = ["1", "2", "3", "4", "5", "6", "7", "9", "0"];
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "9", "0", "."];
     const operators = ["+", "-", "x", "*", "/", "="];
     const functions = ["c", "Backspace", "(", ")", "%"];
     const validKeys = numbers.concat(operators, functions);
-    const lineBreak = document.createElement("br");
+
+    const handleKey = (key) => {
+        if (key === "c") {
+            setOperation("");
+        } else if (key === "Backspace") {
+            setOperation(operation.slice(0, -1));
+        } else if (key === "=") {
+            setOperation(eval(operation));
+        } else if (key === "%") {
+            setOperation(operation + "*0.01");
+        } else if (key === "(") {
+            setOperation(operation + "*(");
+        } else if (key === ")") {
+            setOperation(operation + ")");
+        } else {
+            setOperation(operation + key);
+        }
+    };
 
     const keyDownHandler = (e) => {
         e.preventDefault();
 
         const pressedKey = e.key;
 
-        if (!validKeys.includes(pressedKey)) {
-            console.log("This is not a valid key");
-            return;
-        }
+        if (!validKeys.includes(pressedKey)) return;
 
-        if (
-            pressedKey !== "=" &&
-            pressedKey !== "c" &&
-            pressedKey !== "Backspace"
-        ) {
-            setOperation(`${operation}${pressedKey}`);
-            setOperation(operation + pressedKey);
-        } else {
-            if (pressedKey === "=") {
-                setOperation(eval(operation));
-            } else if (pressedKey === "c") {
-                setOperation("");
-            } else if (pressedKey === "Backspace") {
-                setOperation(operation.slice(0, -1));
-            }
-        }
-
-        // if the number of characters in the operation is greater than 15, the breakLine element is added to the operation input element
-        if (operation.length >= 15) {
-            document
-                .getElementsByClassName("operation-container")[0]
-                .appendChild(lineBreak);
-        }
+        handleKey(pressedKey);
     };
 
     return (
@@ -51,9 +43,7 @@ const Display = () => {
                     className="operation"
                     onKeyDown={(e) => keyDownHandler(e)}
                 ></textarea>
-                <div className="operation-input">
-                    <p className="operation-container">{operation}</p>
-                </div>
+                <div className="operation-input"></div>
             </div>
         </div>
     );
